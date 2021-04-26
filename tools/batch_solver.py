f4b98145f316bb58309b850564943317784c03dc
Aaron Gorenstein <agorenst@outlook.com>
2021-04-26 11:00:25 -0400

    Expanding search scripts
    Building up tools to investigate perfect matchings in graphs via SAT
    reduction

--- tools/batch_solver.py
+++ tools/batch_solver.py
@@ -1,8 +1,20 @@
 import re
 import subprocess
 import sys
+import argparse
 # Takes a solver name and input stream, and looks for "=+\n" (so to speak)
 # that delimits the CNFs provided over the input stream, and feeds it in.
+
+parser = argparse.ArgumentParser(description='Run a sat solver many times')
+parser.add_argument('--print-unsat', action='store_true',
+                    help='Print out CNFs for which we are unsat')
+parser.add_argument('--print-unsat-index', action='store_true',
+                    help='Print out the index of the currently-processing input when its unsat')
+parser.add_argument('--print-index', action='store_true',
+                    help='Print out the index of the currently-processing input')
+parser.add_argument('--start-index', metavar="S", type=int, default=0,
+                    help='What index should we start processing the batch (i.e., how many inputs should we skip)')
+args = parser.parse_args()
 
 solver = './../build/sat'
 delimeter = re.compile('^=+$')
@@ -20,14 +32,36 @@ def isolate_stream(infile):
 
 def run_solver_on_input(cnf_string, solver_name='./../build/sat', flags=[]):
     p = subprocess.Popen(
-        [solver_name] + flags, stdin=subprocess.PIPE)
+        [solver_name] + flags, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    # [solver_name] + flags, stdin=subprocess.PIPE)
     # [solver_name] + flags, stdin=subprocess.PIPE, stdout=subprocess.DEVNULL)
-    p.communicate(cnf_string.encode())
+    (o, _) = p.communicate(cnf_string.encode())
+    return o.decode().strip()
+
+
+def validate_pm_reduction_assumption(cnf_string):
+    real_answer = run_solver_on_input(cnf_string)
+    candidate_answer = run_solver_on_input(
+        cnf_string, flags=["--only-positive-choices"])
+    assert(real_answer == candidate_answer)
 
 
 if __name__ == "__main__":
+    c = 0
     for cnf in isolate_stream(sys.stdin):
-        # Skip empty ones
+        # Skip empty ones, don't count them
         if cnf.strip() == '':
             continue
-        run_solver_on_input(cnf)
+        if c < args.start_index:
+            c += 1
+            continue
+
+        result = run_solver_on_input(cnf)
+        if args.print_unsat_index and result == 'UNSATISFIABLE':
+            print('count:', c)
+        if args.print_unsat and result == 'UNSATISFIABLE':
+            print(cnf)
+
+        # Note count is always yeah
+        c += 1
+        # validate_pm_reduction_assumption(cnf)
--- tools/graph_enum.py
+++ tools/graph_enum.py
@@ -1,8 +1,26 @@
+import sys
 import argparse
+import subprocess
 import itertools
+
 # For my experiments, let's enumerate every labeled graph of size n
 
-n = 6
+parser = argparse.ArgumentParser(description='Generate graphs')
+parser.add_argument('--number-of-nodes', metavar="N", type=int, default='8',
+                    help='The number of nodes in the generated graph')
+parser.add_argument('--produce-one-graph', action='store_true',
+                    help='Different mode, where we produce 1 graph of size N')
+parser.add_argument('--seed', metavar="S", type=int, default=0,
+                    help='Random number generator seed')
+parser.add_argument('--dot', action='store_true',
+                    help='Output graphviz format for (single) graph')
+parser.add_argument('--reduce-graph', action='store_true',
+                    help='Reduce graph fed in from stdin')
+
+args = parser.parse_args()
+
+n = args.number_of_nodes
+
 
 # Define all possible edges (given n) and map each one to a CNF variable
 edges = [(i, j) for i in range(n) for j in range(n) if j > i]
@@ -22,8 +40,11 @@ def incident_edges(g, u):
 
 # Given a graph, print the reduction of its PM problem to CNF
 def reduction(g):
-    n = max([max(i, j) for (i, j) in g])
-    for u in range(n):
+    ns = set([])
+    for u, v in g:
+        ns.add(u)
+        ns.add(v)
+    for u in ns:
         match_me_clause = ' '.join(reduction_map[e]
                                    for e in incident_edges(g, u))
         match_me_clause += ' 0'
@@ -36,7 +57,66 @@ def reduction(g):
                 print('-'+reduction_map[e] + ' -'+reduction_map[h] + ' 0')
 
 
-for e in range(n-1, len(edges)+1):
+def graph_as_dot(g):
+    global reduction_map
+    s = ''
+    s += 'graph {\n'
+    for u, v in g:
+        s += str(u) + ' -- ' + str(v) + '[label='+reduction_map[(u, v)]+'];\n'
+    s += '}\n'
+    return s
+
+
+def make_graph_pdf(g):
+    s = graph_as_dot(g)
+    p = subprocess.Popen(
+        ['dot'] + ['-Tpdf', '-otmp.pdf'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    p.communicate(s.encode())
+
+
+def read_graph(infile):
+    global reduction_map
+    reduction_map.clear()
+    g = []
+    c = 1
+    for line in infile:
+        line = line.strip()
+        if line == '':
+            continue
+        if line[0] == '#':  # support comments
+            continue
+        nodes = map(int, line.split())
+        assert(len(nodes) == 2)
+        u = min(nodes)
+        v = max(nodes)
+        g.append((u, v))
+        reduction_map[(u, v)] = str(c)
+        c += 1
+    if args.dot:
+        make_graph_pdf(g)
+    return g
+
+
+def produce_one_graph():
+    e = int((len(edges)+1) / 2)
+    c = 0
     for g in itertools.combinations(edges, e):
+        if c == args.seed:
+            reduction(g)
+            if args.dot:
+                make_graph_pdf(g)
+            break
+        c += 1
+
+
+if __name__ == "__main__":
+    if args.produce_one_graph:
+        produce_one_graph()
+    elif args.reduce_graph:
+        g = read_graph(sys.stdin)
         reduction(g)
-        print('=============')
+    else:
+        for e in range(n-1, len(edges)+1):
+            for g in itertools.combinations(edges, e):
+                reduction(g)
+                print('=============')
